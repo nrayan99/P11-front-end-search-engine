bf3219cba5a293c53e8c64c27348b113e9ea1b9e
Rayan NAHI <rayan.nahi99@gmail.com>
2022-03-26 06:36:49 +0100

    open close tags

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,4 +1,11 @@
 tagsButtons = document.querySelectorAll('.tags-btn')
+closeTagButtons = document.querySelectorAll('.tags-opened i')
+console.log(closeTagButtons)
+closeTagButtons.forEach(closeTagButton => {
+    closeTagButton.addEventListener('click', function(){
+        closeTag(closeTagButton.className.split(' ')[0].split('-')[1])
+    })
+})
 
 tagsButtons.forEach(tagsButton => {
     tagsButton.addEventListener('click' , function() {
@@ -6,9 +13,11 @@ tagsButtons.forEach(tagsButton => {
     })
 })
 
-
-const ingredientsTagsItems = document.querySelector('.ingredients-opened .tags-items')
-
+const tagsItems = {
+    ingredientsTagsItems: document.querySelector('.ingredients-opened .tags-items'),
+    ustensilsTagsItems: document.querySelector('.ustensils-opened .tags-items'),
+    appliancesTagsItems: document.querySelector('.appliances-opened .tags-items')
+}
 function openTag(tag) {
     console.log(tag)
     const tagToOpen = document.querySelector('.'+ tag + '-opened' )
@@ -17,25 +26,54 @@ function openTag(tag) {
     buttonToHide.style.setProperty('display','none', 'important')
 }
 
-function test() {
-    const ustensilsList = new Set();
-    recipes.forEach(recipe =>{
-        recipe.ingredients.forEach(ingredient=> {
-            ustensilsList.add(ingredient.ingredient)
+function closeTag(tag){
+    console.log(tag)
+    const tagToHide = document.querySelector('.'+ tag + '-opened' )
+    const buttonToShow = document.querySelector('.'+ tag + '-btn' )
+    tagToHide.style.display = 'none'
+    buttonToShow.style.setProperty('display','block', 'important')
+}
+
+function test(tag) {
+    const tagsItemsList = new Set();
+    if (tag === 'ingredients'){
+        recipes.forEach(recipe =>{
+            recipe.ingredients.forEach(ingredient=> {
+                tagsItemsList.add(ingredient.ingredient)
+            })
         })
-        
-        
-    } )
-    ustensilsList.forEach(ustensil => {
-        fillTags(ustensil)
-        console.log(ustensil)
+    }
+    else if (tag === 'appliances') {
+        recipes.forEach(recipe =>{
+            tagsItemsList.add(recipe.appliance)
+        })
+    }
+    else if (tag === 'ustensils') {
+        recipes.forEach(recipe =>{
+            recipe.ustensils.forEach(ustensil=> {
+                tagsItemsList.add(ustensil)
+            })
+        })
+    }
+    tagsItemsList.forEach(tagsItem => {
+        fillTags(tagsItem, tag)
     })
 }
 
-function fillTags(ustensil) {
+function fillTags(tagsItem, tag) {
     const elt = document.getElementById('tags-item-model');
     const dupNode = document.importNode(elt.content,true);
-    dupNode.querySelector('.tags-item-name').textContent = ustensil;
-    ingredientsTagsItems.appendChild(dupNode)
+    dupNode.querySelector('.tags-item-name').textContent = tagsItem;
+    dupNode.querySelector('.tags-item-name').addEventListener('click', function() {
+        selectTag(tagsItem, tag)
+    })
+    tagsItems[tag+'TagsItems'].appendChild(dupNode)
 }
-test();
+
+function selectTag(ustensil){
+    console.log(ustensil)
+}
+
+test('ingredients');
+test('appliances');
+test('ustensils');
